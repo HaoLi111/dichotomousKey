b94c9bfa82b697f2917e53beeb99c0bfcd4322f2
Hao <32727671+HaoLi111@users.noreply.github.com>
2019-02-20 03:32:01 +1300

    add list_to_dk , change dk_as_list to expand

--- R/dk_as_list.R
+++ R/dk_as_list.R
@@ -9,18 +9,22 @@ dk_as_list = function(x=dk_eg,st = 1,asp = 2:3){
       #message(ref)
       stopifnot(!missing(ref))
       if(ref==0){
-        l[[i]] = flow[i,asp]#classification stop
-      }else
-        if(ref==-1){
+        l[[i]] = list()#classification stop
+        l[[i]]$pause = T
+        #l = as.data.frame(l)
+      }else if(ref==-1){
           #link to exterior
-        }else{
+      }else{
           #normal ref, link to id
           l[[i]] = dk_as_list(x,ref,asp)
+          l[[i]]$pause = F
         }
       l[[i]]$prim<- flow[i,asp,drop = F]
+      #l[[i]]=as.list(l[[i]])
       stopifnot(!missing(ref))
     }
   }
+  if(st==1) l$pause = F
   l
 }
 #l = dk_as_list(dk_exp);l;str(l)
---
+++ R/list_as_dk.R
@@ -0,0 +1,65 @@
+#list_to_dk
+list_as_dk = function(x){
+  #x must be a convertible key
+  #if(!length(x)==2) warning("Not a dk-convertible list")
+
+  if(is.null(x[['prim']])) x$prim = data.frame(P =deparse(substitute(x)),
+                                          G = deparse(substitute(x)))
+  sub = x
+  id=1
+  ref=2
+  df =NULL
+  r = 0#row index
+
+  id_adder = 0
+
+  while(length(sub)>2){
+    for(i in 1:(length(sub)-2)){
+
+    prim = unlist(as.vector(sub[[i]]['prim']))
+    rownames(prim)=NULL
+    prim = prim[1:2,drop=T]
+    if(length(sub[[i]])>2){
+        df=rbind(df,c(id,prim,ref))#branch
+
+        ref=ref+1
+    }else{
+      #end p
+       df = rbind(df,c(id,prim,0))#no branch
+    }
+    if(id_adder ==1){
+      id = id+1
+      id_adder=0
+    }else if(id_adder == 0){
+      id_adder = 1
+    }
+    }
+    #
+    #upto
+    #message('com')
+  #id =id+i
+    #r=r+i
+    #id=ref+i
+    sub2 = list()# =list(sub[[1]])
+    i2=1
+
+    for(i in 1:length(sub)){
+      if(length(sub[[i]])==4){
+        #length(sub[[i]])==3 && is.list(sub[[i]]) && sub[[i]]$pause == F){
+        sub2[[i2]] = sub[[i]][[1]]
+        sub2[[i2+1]] = sub[[i]][[2]]
+        i2=i2+2
+      }
+    }
+   #
+    sub=sub2
+    sub['prim'] = 'flow rehandling sublist'
+    sub['pause'] =FALSE
+   # print(df)
+   # print(sub)
+  }#
+
+  colnames(df) = c('id','P','G','ref')
+
+  return(df)
+}
